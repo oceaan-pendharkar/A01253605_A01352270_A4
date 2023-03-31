c57998d8002a2680c60d795e94a0b5650e789cc5
MARTIN SIU <msiu28@my.bcit.ca>
2023-03-31 13:51:11 -0700

    Create deal_damage function and test in battle function

--- Martn.py
+++ Martn.py
@@ -19,19 +19,29 @@ def luck_roll(luck, lower, upper, luck_multiplier=0):
 
 
 def check_first(character, enemy):
-    if character['speed'] + luck_roll(character['luck'], 1, 3, 0.3) >= enemy['speed'] + luck_roll(0, 1, 3):
+    if character['Speed'] + luck_roll(character['Luck'], 1, 3, 0.3) >= enemy['Speed'] + luck_roll(0, 1, 3):
         return [character, enemy]
-    if character['speed'] + luck_roll(character['luck'], 1, 3, 0.3) < enemy['speed'] + luck_roll(0, 1, 3):
+    if character['Speed'] + luck_roll(character['Luck'], 1, 3, 0.3) < enemy['Speed'] + luck_roll(0, 1, 3):
         return [enemy, character]
+
+
+def deal_damage(speed_list):
+    faster = speed_list[0]
+    slower = speed_list[1]
+    slower['Frustration'] -= faster['Intelligence']
 
 
 def battle(character):
     enemy = determine_enemy()
-    # first_to_strike = check_first(character, enemy)
-    # while character['Frustration'] is not 0 or enemy['Frustration'] is not 0:
-        # deal_damage(first_to_strike)
+    first_to_strike = check_first(character, enemy)
+    print(first_to_strike)
+    # while character['Frustration'] <= 0 or enemy['Frustration'] <= 0:
+    deal_damage(first_to_strike)
+    print(character)
+    print(enemy)
+    print(first_to_strike.reverse())
         # is_alive()
-        # deal_damage(slower, faster)
+        # deal_damage(first_to_strike.reverse())
         # is_alive()
 
 
@@ -39,7 +49,7 @@ def main():
     """
     Drive the program.
     """
-    character = {'Motivation': 100, 'Frustration': 75, 'Intelligence': 10, 'Speed': 8, 'luck': 5}
+    character = {'Motivation': 100, 'Frustration': 75, 'Intelligence': 10, 'Speed': 8, 'Luck': 5}
     battle(character)
 
 
