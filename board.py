01880c75b45a975a281b05e58f6e4e545a2becb8
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-04-01 15:21:38 -0700

    Write generate_room and enter_room

--- board.py
+++ board.py
@@ -1,34 +1,63 @@
 import itertools
 import pprint
-from Martn import luck_roll
+import random
+from Martn import battle
 
 LOCATIONS = ['Some BCIT Classroom', 'Tim Hortons', "McDonald's", 'Home',
              'Granville Station', 'Waterfront Station', 'Pacific Centre',
              'Levels Nightclub', 'Nemesis Coffee', 'Kita No Donburi']
 
 
-def generate_classroom() -> None:
+def generate_room(description: str, percent: int, event: str) -> bool:
     """
-    Generate some BCIT classroom for a player to interact with in a game.
+    Generate a room for a player to interact with in a game.
 
+    :param description: the description of the room, as a string
+    :param percent: the percent chance an event will happen in the room, as an integer
+    :param event: the event that might happen in that room
+    :precondition: description must be one of the strings in LOCATIONS
+    :precondition: percent must be an integer
+    :precondition: event must be a string
+    :precondition: attribute must be a string that exists in the character's dictionary keys
+    :precondition: character must be a dictionary
     :postcondition: the player interacts with the room
+    :return: True if the event happens, else False
     """
-    print("You're in some BCIT classroom. There is a 10% chance you'll get assigned ANOTHER assignment.")
+    print(f"You're in {description}. There is a {percent}% chance you will {event}.")
+    number = random.randint(1, percent)
+    guess = input(f"Type an integer [1, {percent}]: ")
+    if number == guess:
+        print(f"You KNEW this would happen! You {event}")
+        return True
+    else:
+        print(f"You did not {event}. As you were...")
+        return False
 
 
-
-def select_room(description: str) -> None:
+def enter_room(character: dict, description: str) -> None:
     """
     Select a specific room for a player to interact with in a game.
 
-    :param description: a string
+    :param character: the character, as a dictionary
+    :param description: a string, the description of the room
     :precondition: description must be a string
+    :precondition: character must be a dictionary
     :postcondition: selects a particular room for a player to interact with in a game
     """
-    if description == LOCATIONS[0]:
-        generate_classroom()
-    elif description == LOCATIONS[1]:
-        # return 10, 'gaining 10 motivation points'
+    if description == LOCATIONS[0] or description == LOCATIONS[3]:
+        event_happens = generate_room(description, 10, 'get assigned ANOTHER assignment')
+        if event_happens:
+            character['Frustration'] += 5
+    elif description == LOCATIONS[1] or description == LOCATIONS[2] or description == LOCATIONS[8]:
+        event_happens = generate_room(description, 20, 'have to fight')
+        if event_happens:
+            battle(character)
+    elif description == LOCATIONS[6] or description == LOCATIONS[7] or description == LOCATIONS[9]:
+        event_happens = generate_room(description, 10, 'gain motivation')
+        if event_happens:
+            character["Motivation"] += 10
+    else:
+        print("Nothing happens in this room. Such is life...")
 
 
 def move_character(character: dict, direction: str) -> None:
