c1d2266b0316fa0e45397a0b55369b70aeed0a8d
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-04-04 17:05:08 -0700

    Edit vitals in game()

--- gameplay.py
+++ gameplay.py
@@ -6,9 +6,8 @@ def game():
     welcome_message()
     game_properties = board.initialize_game(board.make_board(10, 10), character.create_character())
     board.enter_room(game_properties[1])
-    alive = True
-    goal_achieved = False
-    while alive and not goal_achieved:
+    vitals = {"alive": True, "goal achieved": False}
+    while vitals["alive"] and not vitals["goal achieved"]:
         board.move_character(game_properties[0], game_properties[1])
             if challenge:
                 battle()
