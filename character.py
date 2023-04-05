cf421ab72af1d8d42f65a21daebd196ccb4b4f6d
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-04-04 17:23:37 -0700

    Edit make_custom_character to use itertools

--- character.py
+++ character.py
@@ -1,3 +1,6 @@
+import itertools
+
+
 def make_custom_character(character: dict) -> None:
     """
     Create a character with custom values for Motivation, Frustration, Self-Control, Intelligence, and Luck attributes.
@@ -5,22 +8,26 @@ def make_custom_character(character: dict) -> None:
     :character: a dictionary of attributes as strings for keys and integers for values
     :precondition: character must be a dictionary
     :precondition: character's attributes must have values of zero to start
-    :postcondition: adds 100 points total to the character's attributes
+    :postcondition: adds 120 points total to the character's attributes
     """
     points = 120
     new_character = character
-    for key in new_character.keys():
+    key_generator = itertools.cycle(list(["Motivation", "Frustration", "Self-control", "Intelligence", "Luck",
+                                          "Speed"]))
+    while points > 0:
+        key = next(key_generator)
         character[key] = int(input(f"How many points do you want to add to your {key}?"))
         points -= new_character[key]
         if points == 0:
             print("You've used all your points!")
             break
         elif points < 0:
-            print("Woah there, that was more than 100 points!! \nSince you cheated, that's all the points you get for "
+            print("Woah there, that was more than 120 points!! \nSince you cheated, that's all the points you get for "
                   "now. \nAnd you can forget about the extra ones you tried to give yourself. \nThat's not how "
                   "operation COMPLETE ASSIGNMENT 4 works...")
             new_character[key] = 0
             break
+        print(f"You have {points} points left to distribute between your attributes.")
 
 
 def populate_points(character: dict, selection: str) -> dict:
@@ -31,7 +38,7 @@ def populate_points(character: dict, selection: str) -> dict:
     :param selection: the character type, as a string of length 1
     :precondition: character must be a dictionary
     :precondition: character's attributes must have zero as values to begin with
-    :postcondition: adds 100 points total to the character's attributes
+    :postcondition: adds 120 points total to the character's attributes
     """
     for key in character.keys():
         character[key] = 15
@@ -56,15 +63,15 @@ def create_character() -> dict:
     :return: the character, as a dictionary
     """
     character = {"Motivation": 0, "Frustration": 0, "Self-control": 0, "Intelligence": 0, "Luck": 0, "Speed": 0,
-                 'Name': input("What's your character's name?")}
+                 'Name': input("What's your character's name? ")}
     choice = input("Would you like to choose how many points to put in each category? y/n")
 
     if choice == 'y':
-        print(f"Alright! You have ***100 points*** to distribute between Motivation, Frustration, Self-Control, "
-              "Intelligence, and Luck.\nMotivation: helps you stay alive\nFrustration: you want to keep this low during"
-              "battle!\nSelf-Control: like defense\nIntelligence: helps you damage your enemies\nLuck: determines how "
-              "likely you are to meet difficult opponents\nYou also have Fitness, which keeps track of your "
-              "experience level (0 for now!).")
+        print(f"Alright! You have ***120 points*** to distribute between Motivation, Frustration, Self-Control, "
+              "Intelligence, Luck, and Speed.\nMotivation: helps you stay alive\nFrustration: you want to keep this "
+              "low during battle!\nSelf-Control: like defense\nIntelligence: helps you damage your enemies\nLuck: "
+              "determines how likely you are to meet difficult opponents\nSpeed: helps you be quicker than your "
+              "enemies!\nYou also have Fitness, which keeps track of your experience level (0 for now!).")
         make_custom_character(character)
 
     else:
