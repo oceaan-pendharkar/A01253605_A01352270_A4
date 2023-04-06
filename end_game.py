a753e55dcde565867048d8fe4d463870d768cdf8
MARTIN SIU <msiu28@my.bcit.ca>
2023-04-06 14:16:20 -0700

    Create is_alive function and add to endgame function

--- end_game.py
+++ end_game.py
@@ -1,15 +1,25 @@
-def endgame():
-    alive = is_alive()
-    if alive:
-        boss_fight()
+def is_alive(character):
+    if character["Motivation"] <= 0:
+        return False
     else:
-        death()
+        return True
+
+
+def endgame(character):
+    alive = is_alive(character)
+    print(alive)
+    # if alive:
+    #     boss_fight()
+    # else:
+    #     death()
 
 
 def main():
     """
     Drive the program.
     """
+    character = {'Motivation': 100, 'Frustration': 75, 'Intelligence': 10, 'Speed': 8, 'Luck': 5}
+    endgame(character)
 
 
 if __name__ == '__main__':
