09111ee3b75f5e3ef28830b4dc9d6e27dbea635a
MARTIN SIU <msiu28@my.bcit.ca>
2023-04-06 14:11:52 -0700

    Create end_game.py and add endgame function

---
+++ end_game.py
@@ -0,0 +1,16 @@
+def endgame():
+    alive = is_alive()
+    if alive:
+        boss_fight()
+    else:
+        death()
+
+
+def main():
+    """
+    Drive the program.
+    """
+
+
+if __name__ == '__main__':
+    main()
