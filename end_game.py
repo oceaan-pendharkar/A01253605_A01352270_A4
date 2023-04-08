0967c627f7e03088d8a31d81ae39f23d5f2f4396
MARTIN SIU <msiu28@my.bcit.ca>
2023-04-07 23:59:40 -0700

    Create boss_fight and mid_boss_event functions

--- end_game.py
+++ end_game.py
@@ -1,8 +1,28 @@
+import battle
+
+
 def is_alive(character):
     if character["Motivation"] <= 0:
         return False
     else:
         return True
+
+
+def mid_boss_event(character, boss):
+    print("An annoying event is happening!")
+    boss["Intelligence"] *= 1.5
+    boss["Speed"] *= 1.5
+    character["Frustration"] += 50
+
+
+def boss_fight(character):
+    boss = {"Name": "Big Bad Boss", "Frustration": 0, "Max_Frustration": 1000, "Intelligence": 69, "Speed": 69}
+    character_is_faster = battle.check_first(character, boss)
+    battle.battle(character_is_faster, character, boss, boss["Max_Frustration"] / 2)
+    if character["Frustration"] < character["Max_Frustration"]:
+        mid_boss_event(character, boss)
+    battle.battle(character_is_faster, character, boss, boss["Max_Frustration"])
+    check_result()
 
 
 def endgame(character):
@@ -18,7 +38,8 @@ def main():
     """
     Drive the program.
     """
-    character = {'Motivation': 100, 'Frustration': 75, 'Intelligence': 10, 'Speed': 8, 'Luck': 5}
+    character = {'Motivation': 100, 'Frustration': 0, "Max_Frustration": 500, 'Intelligence': 100,
+                 'Speed': 85, 'Luck': 45}
     endgame(character)
 
 
