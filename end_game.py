4b3b45ef1f9cc89f31a61516f217a3fa5465133a
MARTIN SIU <msiu28@my.bcit.ca>
2023-04-08 16:26:30 -0700

    Add boss_lose and boss_win functions

--- end_game.py
+++ end_game.py
@@ -15,21 +15,32 @@ def mid_boss_event(character, boss):
     character["Frustration"] += 50
 
 
+def boss_lose(character, enemy):
+    print(f"{enemy['Name']} has frustrated you so much, that you just gave up. You lost 30 motivation. You decided to "
+          f"go for a walk and ended up near the school")
+    character["Motivation"] -= 30
+    go_for_a_walk()
+
+
+def boss_win(character, enemy):
+    print(f"Congratulations! You've beaten {enemy['Name']} and have completed the game!")
+
+
 def boss_fight(character):
-    boss = {"Name": "Big Bad Boss", "Frustration": 0, "Max_Frustration": 1000, "Intelligence": 69, "Speed": 69}
+    boss = {"Name": "Assignment 1", "Frustration": 0, "Max_Frustration": 1000, "Intelligence": 69, "Speed": 69, "Self-Control": 69}
     character_is_faster = battle.check_first(character, boss)
     battle.battle(character_is_faster, character, boss, boss["Max_Frustration"] / 2)
     if character["Frustration"] < character["Max_Frustration"]:
         mid_boss_event(character, boss)
     battle.battle(character_is_faster, character, boss, boss["Max_Frustration"])
-    check_result()
+    battle.check_result(character, boss, boss_lose, boss_win)
 
 
 def endgame(character):
     alive = is_alive(character)
     print(alive)
-    # if alive:
-    #     boss_fight()
+    if alive:
+        boss_fight(character)
     # else:
     #     death()
 
@@ -39,7 +50,7 @@ def main():
     Drive the program.
     """
     character = {'Motivation': 100, 'Frustration': 0, "Max_Frustration": 500, 'Intelligence': 100,
-                 'Speed': 85, 'Luck': 45}
+                 'Speed': 85, "Self-Control": 5, 'Luck': 45}
     endgame(character)
 
 
