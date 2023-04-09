1818821cb2919e3cf9a93ec369bb05ce75ef91e6
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-04-08 17:53:30 -0700

    Edit game loop to include vitals as attributes

--- gameplay.py
+++ gameplay.py
@@ -7,8 +7,7 @@ def game():
     game_properties = board.initialize_game(board.make_board(10, 10), character.create_character())
     board.welcome_message(game_properties[1])
     board.enter_room(game_properties[1])
-    vitals = {"alive": True, "goal achieved": False}
-    while vitals["alive"] and not vitals["goal achieved"]:
+    while game_properties[1]["alive"] and not game_properties[1]["goal achieved"]:
         board.move_character(game_properties[0], game_properties[1])
         board.enter_room(game_properties[1])
         character.check_alive(game_properties[1])
