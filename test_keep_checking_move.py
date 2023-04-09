4ff420c5256c0523c9a0a4023241cd6951652299
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-04-09 15:46:43 -0700

    Write unit tests for keep_checking_move

--- test_keep_checking_move.py
+++ test_keep_checking_move.py
@@ -1,6 +1,7 @@
 import io
 from unittest import TestCase
 from unittest.mock import patch
+
 from board import keep_checking_move
 
 
@@ -8,6 +9,12 @@ class Test(TestCase):
     @patch('builtins.input', side_effect=['n'])
     def test_valid_north(self, _):
         self.assertEqual(keep_checking_move(((0, 5), (0, 5)), {"row": 2, "column": 1}), 'n')
+
+    @patch('builtins.input', side_effect=['n', 's'])
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_invalid_north(self, mock_output, _):
+        keep_checking_move(((0, 5), (0, 5)), {"row": 0, "column": 1})
+        self.assertEqual(mock_output.getvalue(), "Your move must stay within the bounds of the board!\n")
 
     @patch('builtins.input', side_effect=['s'])
     def test_valid_south(self, _):
@@ -28,11 +35,8 @@ class Test(TestCase):
         self.assertEqual(mock_output.getvalue(), "Direction must be 'n', 's', 'e', or 'w'!\n")
 
     @patch('builtins.input', side_effect=['2', 'i', '9', 's'])
-    def test_loops(self, _):
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_loops(self, mock_output, _):
         self.assertEqual(keep_checking_move(((0, 5), (0, 5)), {"row": 2, "column": 1}), 's')
-
-
-
-
-
-
+        self.assertEqual(mock_output.getvalue(), "Direction must be 'n', 's', 'e', or 'w'!\nDirection must be 'n', "
+                                                 "'s', 'e', or 'w'!\nDirection must be 'n', 's', 'e', or 'w'!\n")
