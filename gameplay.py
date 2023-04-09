af20e572e4911413a5c40863b7005f88e840e708
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-04-08 17:32:02 -0700

    Call enter_room in game loop

--- gameplay.py
+++ gameplay.py
@@ -10,6 +10,7 @@ def game():
     vitals = {"alive": True, "goal achieved": False}
     while vitals["alive"] and not vitals["goal achieved"]:
         board.move_character(game_properties[0], game_properties[1])
+        board.enter_room(game_properties[1])
         character.check_alive(game_properties[1])
         character.check_goal(game_properties[1])
     end_game.endgame(game_properties[1])
