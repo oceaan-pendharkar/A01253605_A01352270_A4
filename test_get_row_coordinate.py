9edeed882d4a9a0c3fa1e99806c5bfd00f228aa2
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-04-09 15:54:53 -0700

    Write south/north tests for get_row_coordinate

---
+++ test_get_row_coordinate.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+from board import get_row_coordinate
+
+
+class Test(TestCase):
+    def test_south(self):
+        character = {"row": 0, "column": 0}
+        self.assertEqual(get_row_coordinate(character, 's'), 1)
+
+    def test_negative_north(self):
+        character = {"row": 0, "column": 0}
+        self.assertEqual(get_row_coordinate(character, 'n'), -1)
+
+    def test_big_south(self):
+        character = {"row": 24, "column": 3}
+        self.assertEqual(get_row_coordinate(character, 's'), 25)
+
+    def test_big_north(self):
+        character = {"row": 24, "column": 3}
+        self.assertEqual(get_row_coordinate(character, 'n'), 23)
