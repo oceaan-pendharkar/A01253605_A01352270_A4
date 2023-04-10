9e1fc86c897ae24992028e625f488e7e3fa0da99
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-04-09 23:22:18 -0700

    Write test_jock

--- Tests/test_make_preset_character.py
+++ Tests/test_make_preset_character.py
@@ -43,3 +43,16 @@ class Test(TestCase):
                           "Speed": 5,
                           "Fitness": 5, 'Name': "Name", "row": 0, "column": 0, "Level": 1,
                           "alive": True, "goal achieved": False})
+
+    @patch('builtins.input', side_effect=['j'])
+    def test_jock(self, _):
+        character = {"Motivation": 10, "Max Frustration": 20, "Self-Control": 5, "Intelligence": 5, "Luck": 5,
+                     "Speed": 5,
+                     "Fitness": 5, 'Name': "Name", "row": 0, "column": 0, "Level": 1,
+                     "alive": True, "goal achieved": False}
+        make_preset_character(character)
+        self.assertEqual(character,
+                         {"Motivation": 10, "Max Frustration": 20, "Self-Control": 5, "Intelligence": 5, "Luck": 5,
+                          "Speed": 15,
+                          "Fitness": 5, 'Name': "Name", "row": 0, "column": 0, "Level": 1,
+                          "alive": True, "goal achieved": False})
