bc3613abb28362827ba6a8e87add6d2c86db5916
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-04-09 23:19:07 -0700

    Write test_nerd

--- Tests/test_make_preset_character.py
+++ Tests/test_make_preset_character.py
@@ -1,9 +1,19 @@
 from unittest import TestCase
+from unittest.mock import patch
+
 from character import make_preset_character
 
 
 class Test(TestCase):
     @patch('builtins.input', side_effect=['n'])
-
-    def test_n(self):
-        self.fail()
+    def test_nerd(self, _):
+        character = {"Motivation": 10, "Max Frustration": 60, "Self-Control": 5, "Intelligence": 5, "Luck": 5,
+                     "Speed": 5,
+                     "Fitness": 5, 'Name': "Name", "row": 0, "column": 0, "Level": 1,
+                     "alive": True, "goal achieved": False}
+        make_preset_character(character)
+        self.assertEqual(character,
+                         {"Motivation": 10, "Max Frustration": 60, "Self-Control": 5, "Intelligence": 15, "Luck": 5,
+                          "Speed": 5,
+                          "Fitness": 5, 'Name': "Name", "row": 0, "column": 0, "Level": 1,
+                          "alive": True, "goal achieved": False})
