808b2e92e8d58a653b2bdc591cd18e0796b05084
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-04-09 22:41:24 -0700

    Remove break statements in populate_custom_points

--- character.py
+++ character.py
@@ -27,11 +27,13 @@ def populate_custom_points(character: dict, points: int) -> None:
 
         if points == 0:
             print("You've used all your points!")
+
         elif points < 0:
             print(f"Woah there, that was more than {points} points!! \nSince you cheated, that's all the points you "
                   "get for now. \nAnd you can forget about getting points for the category you just over-filled. "
                   "\nThat's not how operation COMPLETE ASSIGNMENT 4 works...")
             character[key] -= point_increase
+
         print(f"You have {points} points left to distribute between your attributes.")
 
 
