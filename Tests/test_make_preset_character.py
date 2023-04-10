8c5cd65847fc90e0bdf24b4249288ca7d9276224
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-04-09 23:24:23 -0700

    Write test_wrong_input

--- Tests/test_make_preset_character.py
+++ Tests/test_make_preset_character.py
@@ -1,3 +1,4 @@
+import io
 from unittest import TestCase
 from unittest.mock import patch
 
@@ -56,3 +57,18 @@ class Test(TestCase):
                           "Speed": 15,
                           "Fitness": 5, 'Name': "Name", "row": 0, "column": 0, "Level": 1,
                           "alive": True, "goal achieved": False})
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch('builtins.input', side_effect=[2, 'j'])
+    def test_wrong_input(self, _, mock_output):
+        character = {"Motivation": 10, "Max Frustration": 33, "Self-Control": 5, "Intelligence": 5, "Luck": 5,
+                     "Speed": 5,
+                     "Fitness": 5, 'Name': "Name", "row": 0, "column": 0, "Level": 1,
+                     "alive": True, "goal achieved": False}
+        make_preset_character(character)
+        self.assertEqual(character,
+                         {"Motivation": 10, "Max Frustration": 33, "Self-Control": 5, "Intelligence": 5, "Luck": 5,
+                          "Speed": 15,
+                          "Fitness": 5, 'Name': "Name", "row": 0, "column": 0, "Level": 1,
+                          "alive": True, "goal achieved": False})
+        self.assertEqual(mock_output.getvalue(), "You must enter 'n', 'l', 'g', or 'j'. Try again...\n")
