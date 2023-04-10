13c90b69e77ea773a091265a09677002b207e24a
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-04-09 23:10:32 -0700

    Edit input in creation of character in populate_custom_character unit tests

--- Tests/test_populate_custom_points.py
+++ Tests/test_populate_custom_points.py
@@ -26,43 +26,49 @@ class Test(TestCase):
     def test_cycle(self, _, mock_output):
         character = {"Motivation": 2, "Max Frustration": 60, "Self-Control": 5, "Intelligence": 5, "Luck": 5,
                      "Speed": 5,
-                     "Fitness": 5, 'Name': input("What's your character's name? "), "row": 0, "column": 0, "Level": 1,
+                     "Fitness": 5, 'Name': 'Name', "row": 0, "column": 0, "Level": 1,
                      "alive": True, "goal achieved": False}
         populate_custom_points(character, 10)
-        self.assertEqual(mock_output.getvalue(), "You have 10 points left to distribute between your attributes.\n"
-                                                 "You have 9 points left to distribute between your attributes.\n"
-                                                 "You have 8 points left to distribute between your attributes.\n"
-                                                 "You have 7 points left to distribute between your attributes.\n"
-                                                 "You have 5 points left to distribute between your attributes.\n"
-                                                 "You have 3 points left to distribute between your attributes.\n"
-                                                 "You have 1 points left to distribute between your attributes.\n"
-                                                 "You've used all your points!\n")
+        self.assertEqual(mock_output.getvalue(),
+                         "You have 9 points left to distribute between your attributes.\n"
+                         "You have 8 points left to distribute between your attributes.\n"
+                         "You have 7 points left to distribute between your attributes.\n"
+                         "You have 5 points left to distribute between your attributes.\n"
+                         "You have 3 points left to distribute between your attributes.\n"
+                         "You have 1 points left to distribute between your attributes.\n"
+                         "You've used all your points!\n"
+                         "You have 0 points left to distribute between your attributes.\n")
 
     @patch('sys.stdout', new_callable=io.StringIO)
     @patch('builtins.input', side_effect=[0, 10])
     def test_early_stop(self, _, mock_output):
         character = {"Motivation": 34, "Max Frustration": 60, "Self-Control": 5, "Intelligence": 5, "Luck": 5,
                      "Speed": 5,
-                     "Fitness": 5, 'Name': input("What's your character's name? "), "row": 0, "column": 0, "Level": 1,
+                     "Fitness": 5, 'Name': 'name', "row": 0, "column": 0, "Level": 1,
                      "alive": True, "goal achieved": False}
         populate_custom_points(character, 10)
         self.assertEqual(mock_output.getvalue(), "You have 10 points left to distribute between your attributes.\n"
                                                  "You've used all your points!\n")
 
     @patch('sys.stdout', new_callable=io.StringIO)
-    @patch('builtins.input', side_effect=[0, 11])
+    @patch('builtins.input', side_effect=[11])
     def test_reprimand(self, _, mock_output):
         character = {"Motivation": 22, "Max Frustration": 60, "Self-Control": 5, "Intelligence": 5, "Luck": 5,
                      "Speed": 5,
-                     "Fitness": 5, 'Name': input("What's your character's name? "), "row": 0, "column": 0, "Level": 1,
+                     "Fitness": 5, 'Name': 'Name', "row": 0, "column": 0, "Level": 1,
                      "alive": True, "goal achieved": False}
         populate_custom_points(character, 10)
-        self.assertEqual(mock_output.getvalue(), "You have 10 points left to distribute between your attributes."
-                                                 "\nWoah there, that was more points than we said!! "
+        self.assertEqual(mock_output.getvalue(), "Woah there, that was more points than we said!! "
                                                  "\nSince you cheated, that's all the points you get for now. "
                                                  "\nAnd you can forget about getting points for the category you just "
                                                  "over-filled. "
-                                                 "\nThat's not how operation COMPLETE ASSIGNMENT 4 works...\n")
+                                                 "\nThat's not how operation COMPLETE ASSIGNMENT 4 works..."
+                                                 "\nYou have -1 points left to distribute between your attributes.\n")
+        self.assertEqual({"Motivation": 22, "Max Frustration": 60, "Self-Control": 5, "Intelligence": 5, "Luck": 5,
+                          "Speed": 5,
+                          "Fitness": 5, 'Name': 'Name', "row": 0, "column": 0,
+                          "Level": 1,
+                          "alive": True, "goal achieved": False}, character)
 
     def test_type_character(self):
         with self.assertRaises(TypeError):
