24eca7978316cbc4edd4e6dd8611d52f16e377a3
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-04-09 22:51:03 -0700

    Write cycle and regular case unit tests for populate_custom_points

---
+++ Tests/test_populate_custom_points.py
@@ -0,0 +1,39 @@
+import io
+from unittest import TestCase
+from unittest.mock import patch
+
+from character import populate_custom_points
+
+
+class Test(TestCase):
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch('builtins.input', side_effect=[0, 2, 2, 2, 2, 2])
+    def test_normal_case(self, _, mock_output):
+        character = {"Motivation": 10, "Max Frustration": 60, "Self-Control": 5, "Intelligence": 5, "Luck": 5,
+                     "Speed": 5,
+                     "Fitness": 5, 'Name': input("What's your character's name? "), "row": 0, "column": 0, "Level": 1,
+                     "alive": True, "goal achieved": False}
+        populate_custom_points(character, 10)
+        self.assertEqual(mock_output.getvalue(), "You have 10 points left to distribute between your attributes.\n"
+                                                 "You have 8 points left to distribute between your attributes.\n"
+                                                 "You have 6 points left to distribute between your attributes.\n"
+                                                 "You have 4 points left to distribute between your attributes.\n"
+                                                 "You have 2 points left to distribute between your attributes.\n"
+                                                 "You've used all your points!\n")
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    @patch('builtins.input', side_effect=[0, 1, 1, 1, 2, 2, 2, 1])
+    def test_cycle(self, _, mock_output):
+        character = {"Motivation": 2, "Max Frustration": 60, "Self-Control": 5, "Intelligence": 5, "Luck": 5,
+                     "Speed": 5,
+                     "Fitness": 5, 'Name': input("What's your character's name? "), "row": 0, "column": 0, "Level": 1,
+                     "alive": True, "goal achieved": False}
+        populate_custom_points(character, 10)
+        self.assertEqual(mock_output.getvalue(), "You have 10 points left to distribute between your attributes.\n"
+                                                 "You have 9 points left to distribute between your attributes.\n"
+                                                 "You have 8 points left to distribute between your attributes.\n"
+                                                 "You have 7 points left to distribute between your attributes.\n"
+                                                 "You have 5 points left to distribute between your attributes.\n"
+                                                 "You have 3 points left to distribute between your attributes.\n"
+                                                 "You have 1 points left to distribute between your attributes.\n"
+                                                 "You've used all your points!\n")
