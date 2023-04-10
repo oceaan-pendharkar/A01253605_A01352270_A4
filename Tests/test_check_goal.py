1c35f8897fd42fdd50a3e9b0bfc8c4523eb472bf
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-04-09 21:42:44 -0700

    Write Type Error unit tests for check_goal

--- Tests/test_check_goal.py
+++ Tests/test_check_goal.py
@@ -3,7 +3,10 @@ from character import check_goal
 
 
 class Test(TestCase):
-    def test_raises_type(self):
-        character = []
+    def test_raises_type_character(self):
         with self.assertRaises(TypeError):
-            check_goal(character)
+            check_goal([], ())
+
+    def test_raises_type_board(self):
+        with self.assertRaises(TypeError):
+            check_goal({}, [])
