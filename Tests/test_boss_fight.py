241861ae6be7d9baadd153df8b4a26f6e65bd74e
MARTIN SIU <msiu28@my.bcit.ca>
2023-04-10 18:41:14 -0700

    Change unit tests to reflect the change

--- Tests/test_boss_fight.py
+++ Tests/test_boss_fight.py
@@ -10,17 +10,17 @@ class Test(TestCase):
         character = {'Name': 'Bob', 'Motivation': 100, 'Frustration': 0, "Max Frustration": 500, 'Intelligence': 100,
                      'Speed': 85, "Self-Control": 5, 'Level': 3, 'Fitness': 30, 'Luck': 45, "row": 85, "column": 85}
         boss_fight(character)
-        expected_character = {'Name': 'Bob', 'Motivation': 100, 'Frustration': 30, "Max Frustration": 500,
+        expected_character = {'Name': 'Bob', 'Motivation': 100, 'Frustration': 10, "Max Frustration": 500,
                               'Intelligence': 100, 'Speed': 85, "Self-Control": 5, 'Level': 3, 'Fitness': 30,
                               'Luck': 45, "row": 85, "column": 85}
         self.assertEqual(expected_character, character)
 
     @patch('random.randint', side_effect=[100, 0, 100, 0, 100, 0, 100, 0, 100, 0, 100, 0])
     def test_character_lose(self, _):
-        character = {'Name': 'Bob', 'Motivation': 100, 'Frustration': 0, "Max Frustration": 40, 'Intelligence': 25,
+        character = {'Name': 'Bob', 'Motivation': 100, 'Frustration': 0, "Max Frustration": 15, 'Intelligence': 25,
                      'Speed': 85, "Self-Control": 5, 'Level': 3, 'Fitness': 30, 'Luck': 45, "row": 85, "column": 85}
         boss_fight(character)
-        expected_character = {'Name': 'Bob', 'Motivation': 100, 'Frustration': 40, "Max Frustration": 40,
+        expected_character = {'Name': 'Bob', 'Motivation': 100, 'Frustration': 18, "Max Frustration": 15,
                               'Intelligence': 25, 'Speed': 85, "Self-Control": 5, 'Level': 3, 'Fitness': 30,
                               'Luck': 45, "row": 85, "column": 85}
         self.assertEqual(expected_character, character)
